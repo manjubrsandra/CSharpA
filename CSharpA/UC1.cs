09877dac847e13a4d744e8f80a12a47d846cda78
manjubrsandra <manjunathbrsandra43@gmail.com>
2022-04-22 12:02:11 +0530

    Attendence

---
+++ CSharpA/UC1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpA
+{
+    internal class UC1
+    {
+        public static void Attendence()
+        {
+            int IS_FULL_TIME = 1;
+            Random obj= new Random();
+            int num= obj.Next(0,2);
+            if(num == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
